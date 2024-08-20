c6c5eec70814e00f85c4f8438019b40384c34f2c
CameronGillum <GillumTCameron@gmail.com>
2024-08-19 22:57:59 -0400

    removes mongodb link to try making server launch

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+// const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(connectionString, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const db = mongoose.connection;
 
